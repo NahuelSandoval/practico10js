ac05b7c9bb24e95d4f27eb0ce90b6583ebf31ad9
NahuelSandoval <apos_facu@hotmail.com>
2022-10-28 22:05:29 -0300

    js

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+const nombre = "Nahuel"
+let apellido
+apellido = "Sandoval"
+console.log(nombre , apellido)
+console.log("holanda")
+
+let numeroA = 5
+let numeroB = 8
+let resultadosuma = numeroA + numeroB;
+console.log(resultadosuma);
+let resultadoresta = numeroA - numeroB
+console.log(resultadoresta)
+const numero3 = 4
+let resultadomulti = resultadosuma * numero3
+
+console.log(resultadomulti)
+
+numeroA = 4
+console.log(nombre , numeroA)
+
+/* alert("preparate para poner tu nombre");
+
+prompt("dale lokita")
+
+let nomb = prompt("pone tu nombre") 
+console.log(nomb) */
+
+let entrada = prompt("Ingresar una letra");
+let salida  = entrada + " " + "ingresada";
+alert(salida);
+
